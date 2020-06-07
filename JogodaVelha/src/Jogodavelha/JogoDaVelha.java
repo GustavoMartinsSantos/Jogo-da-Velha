0ab84c1be2dade9a96134d28b0af481b0c7bbd32
GustavoMartinsdosSantos <gustavo_mts2005@hotmail.com>
2020-06-07 16:30:24 -0300

    Adding a loop that offers the option to continue playing

--- JogodaVelha/src/Jogodavelha/JogoDaVelha.java
+++ JogodaVelha/src/Jogodavelha/JogoDaVelha.java
@@ -27,106 +27,111 @@ public class JogoDaVelha {
 			System.out.println("");
 		}
 		// impressão das posições da matriz
+		String option = "1";
 		
 		Scanner input = new Scanner (System.in);
 		try {
-			for(int y = 1; y <= 5; y++) { //irá ocorrer 9 vezes
-				System.out.print("Digite a posição da O: "); 
+			while (option.equals("1")) {
+				for(int y = 1; y <= 5; y++) { 
+					System.out.print("Digite a posição da O: "); 
+						
+					int o = input.nextInt(); 
 					
-				int o = input.nextInt(); 
+					while (o <= 0 || o > 9) {
+						System.out.print("Você digitou o número errado. Tente novamente: ");
+						o = input.nextInt();
+					} // mensagem de erro
+						
+					switch(o) { // caso o valor da o for de 1 a 9, então será substituído
+						case 1: 
+							vetor[0][0] = "_O_";
+							break;
+						case 2:
+							vetor[0][1] = "_O_";
+							break;
+						case 3:
+							vetor[0][2] = "_O_";
+							break;
+						case 4:
+							vetor[1][0] = "_O_";
+							break;
+						case 5:
+							vetor[1][1] = "_O_";
+							break;
+						case 6:
+							vetor[1][2] = "_O_";
+							break;
+						case 7:
+							vetor[2][0] = "_O_";
+							break;
+						case 8:
+							vetor[2][1] = "_O_";
+							break;
+						case 9:
+							vetor[2][2] = "_O_";
+							break;
+					}
+						
+					System.out.println("Jogo da Velha"); 
+					for(l = 0; l < 3; l++) {
+						for(c = 0; c < 3; c++)
+							System.out.print(vetor[l][c]);
+						System.out.println("");
+					}
+					// imprimir o novo valor da matriz
 				
-				while (o <= 0 || o > 9) {
-					System.out.print("Você digitou o número errado. Tente novamente: ");
-					o = input.nextInt();
-				} // mensagem de erro
 					
-				switch(o) { // caso o valor da o for de 1 a 9, então será substituído
-					case 1: 
-						vetor[0][0] = "_O_";
-						break;
-					case 2:
-						vetor[0][1] = "_O_";
-						break;
-					case 3:
-						vetor[0][2] = "_O_";
-						break;
-					case 4:
-						vetor[1][0] = "_O_";
-						break;
-					case 5:
-						vetor[1][1] = "_O_";
-						break;
-					case 6:
-						vetor[1][2] = "_O_";
-						break;
-					case 7:
-						vetor[2][0] = "_O_";
-						break;
-					case 8:
-						vetor[2][1] = "_O_";
-						break;
-					case 9:
-						vetor[2][2] = "_O_";
-						break;
-				}
+					if (y == 5)  // se o contador de y chegar a 5 ele para a linha de execução abaixo
+						continue;
 					
-				System.out.println("Jogo da Velha"); 
-				for(l = 0; l < 3; l++) {
-					for(c = 0; c < 3; c++)
-						System.out.print(vetor[l][c]);
-					System.out.println("");
+					System.out.print("Digite a posição do X: "); 
+					int x = input.nextInt();
+					
+					while (x <= 0 || x > 9) {
+						System.out.print("Você digitou o número errado. Tente novamente: ");
+						x = input.nextInt();
+					} // mensagem de erro
+					
+					switch(x) {
+						case 1: 
+							vetor[0][0] = "_X_";
+							break;
+						case 2:
+							vetor[0][1] = "_X_";
+							break;
+						case 3:
+							vetor[0][2] = "_X_";
+							break;
+						case 4:
+							vetor[1][0] = "_X_";
+							break;
+						case 5:
+							vetor[1][1] = "_X_";
+							break;
+						case 6:
+							vetor[1][2] = "_X_";
+							break;
+						case 7:
+							vetor[2][0] = "_X_";
+							break;
+						case 8:
+							vetor[2][1] = "_X_";
+							break;
+						case 9:
+							vetor[2][2] = "_X_";
+							break;
+					}
+					
+					System.out.println("Jogo da Velha");
+					for(l = 0; l < 3; l++) {
+						for(c = 0; c < 3; c++)
+							System.out.print(vetor[l][c]);
+						System.out.println("");
+					}
+					// impressão da matriz
 				}
-				// imprimir o novo valor da matriz
-			
-				
-				if (y == 5)  // se o contador de y chegar a 5 ele para a linha de execução abaixo
-					continue;
-				
-				System.out.print("Digite a posição do X: "); 
-				int x = input.nextInt();
-				
-				while (x <= 0 || x > 9) {
-					System.out.print("Você digitou o número errado. Tente novamente: ");
-					x = input.nextInt();
-				} // mensagem de erro
-				
-				switch(x) {
-					case 1: 
-						vetor[0][0] = "_X_";
-						break;
-					case 2:
-						vetor[0][1] = "_X_";
-						break;
-					case 3:
-						vetor[0][2] = "_X_";
-						break;
-					case 4:
-						vetor[1][0] = "_X_";
-						break;
-					case 5:
-						vetor[1][1] = "_X_";
-						break;
-					case 6:
-						vetor[1][2] = "_X_";
-						break;
-					case 7:
-						vetor[2][0] = "_X_";
-						break;
-					case 8:
-						vetor[2][1] = "_X_";
-						break;
-					case 9:
-						vetor[2][2] = "_X_";
-						break;
-				}
-				
-				System.out.println("Jogo da Velha");
-				for(l = 0; l < 3; l++) {
-					for(c = 0; c < 3; c++)
-						System.out.print(vetor[l][c]);
-					System.out.println("");
-				}
-				// impressão da matriz
+				System.out.print("Digite 1 para continuar jogando: ");
+				option = input.next();
 			}
 		}
 		catch (InputMismatchException e) {
@@ -137,7 +142,8 @@ public class JogoDaVelha {
 			System.out.println("ERRO!");
 			e.printStackTrace();
 		}
-			
+		
+		System.out.println("Obrigado por testar o programa!");	
 		input.close();
 	}
 	
